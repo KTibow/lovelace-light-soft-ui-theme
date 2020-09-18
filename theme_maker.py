ffa36d931419ba8774a867d0442a3dc6707a632a
Kendell R <KTibow@users.noreply.github.com>
2020-09-17 19:43:50 -0700

    Update colors

--- theme_maker.py
+++ theme_maker.py
@@ -27,8 +27,7 @@ for color_name, color_hue in [
     primary_color = convert_color(primary_color)
     accent_color = colorsys.hsv_to_rgb(((color_hue - 25) % 360) / 360.0, 0.59, 0.92)
     accent_color = convert_color(accent_color)
-    background_color = colorsys.hsv_to_rgb(color_hue / 360.0, 0.03, 0.97)
-    # background_color = colorsys.hsv_to_rgb(color_hue / 360.0, 0.13, 0.22)
+    background_color = colorsys.hsv_to_rgb(color_hue / 360.0, 0.02, 0.97)
     background_color = convert_color(background_color)
     file_color_name = color_name.lower().replace(" ", "-")
     with open(rf"themes\light-soft-ui-{file_color_name}.yaml", "w") as theme_file:
