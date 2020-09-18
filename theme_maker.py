ce4c9d69379ee47a83e07868a627b69b8cef84cd
Kendell R <KTibow@users.noreply.github.com>
2020-09-17 18:05:36 -0700

    update themes

--- theme_maker.py
+++ theme_maker.py
@@ -1,42 +1,43 @@
 import colorsys
 
+
+def convert_color(color):
+    color = [round(comp * 255.0) for comp in color]
+    color = "".join([format(comp, "x") for comp in color])
+    return color.zfill(6)
+
+
 theme = open("theme.yaml").read()
 for color_name, color_hue in [
-    ["Reddish Pink", 335],
-    ["Pinkish Red", 345],
-    ["Pale Red", 355],
-    ["Red", 5],
-    ["Red Orange", 15],
-    ["Orange", 25],
-    ["Gold Orange", 35],
-    ["Gold", 45],
-    ["Yellow", 55],
-    ["Yellow Lime", 65],
-    ["Lime Yellow", 75],
-    ["Lime", 85],
-    ["Lime Green", 95],
-    ["Green Lime", 105],
-    ["Green", 115],
-    ["Green Mint", 125],
-    ["Mint Green", 135],
-    ["Mint", 145],
-    ["Mint Cyan", 155],
-    ["Cyan Mint", 165],
-    ["Cyan", 175],
-    ["Cyan Blue", 185],
-    ["Blue Cyan", 195],
-    ["Light Blue", 205],
-    ["Lightish Blue", 215],
-    ["Blue", 225],
-    ["Blue Indigo", 235],
-    ["Indigo", 245],
-    ["Purple", 255],
-    ["Lavender Purple", 265],
-    ["Lavender", 275],
-    ["Lavender Pink", 285],
-    ["Pink Lavender", 295],
-    ["Pink", 305],
-    ["Pink Red", 315],
-    ["Red Pink", 325],
+    ["Red", 0],
+    ["Fire", 18],
+    ["Orange", 36],
+    ["Yellow", 54],
+    ["Yellow Green", 79],
+    ["Green Yellow", 104],
+    ["Green", 129],
+    ["Mint", 155],
+    ["Cyan", 180],
+    ["Light Blue", 200],
+    ["Blue", 214],
+    ["Dark Blue", 240],
+    ["Indigo", 260],
+    ["Purple", 275],
+    ["Pink", 300],
 ]:
-    pass
+    primary_color = colorsys.hsv_to_rgb(color_hue / 360.0, 0.59, 0.92)
+    primary_color = convert_color(primary_color)
+    accent_color = colorsys.hsv_to_rgb(((color_hue - 25) % 360) / 360.0, 0.59, 0.92)
+    accent_color = convert_color(accent_color)
+    background_color = colorsys.hsv_to_rgb(color_hue / 360.0, 0.03, 0.97)
+    background_color = convert_color(background_color)
+    # background_color = colorsys.hsv_to_rgb(color_hue / 360.0, 0.13, 0.22)
+    with open(
+        rf"themes\soft-ui-{color_name.lower().replace(' ', '-')}.yaml", "w"
+    ) as theme_file:
+        theme_file.write(
+            theme.replace("{color_name}", color_name)
+            .replace("{primary_color}", primary_color)
+            .replace("{accent_color}", accent_color)
+            .replace("{background_color}", background_color)
+        )
